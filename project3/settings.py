0bd95cb1e07c63b9984b0511bd8170d982df82a1
Santiago Gonzalez Volpe <sgvolpe@gmail.com>
2019-02-18 20:03:54 +0000

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    
     'accounts',
     'bootstrap3',
     
